1d501043b94284ee3846bff1f8abbc498b1b7408
Boreas618 <21307130094@m.fudan.edu.cn>
2023-05-25 19:47:10 +1000

    Add README.md

---
+++ README.md
@@ -0,0 +1,19 @@
+# Eash Conor Case Inspector
+![Python](https://img.shields.io/badge/python-v3.x-blue)
+
+This is the backend for the Eash Conor Case Inspector. It is a FastAPI application that supports discovering conor cases for autonomous driving from transportation data.
+
+
+## Installation
+
+```bash
+$ git clone https://github.com/Boreas618/Easy-Cornor-Case-Inspector.git
+$ cd Easy-Cornor-Case-Inspector
+$ pip install -r requirements.txt
+```
+
+## Usage
+
+```bash
+$ uvicorn main:app --reload
+```
--- utils/data_utils.py
+++ utils/data_utils.py
@@ -1,4 +1,5 @@
 import json
+import datetime
 
 
 def load_data():
@@ -14,3 +15,27 @@ def load_data():
                 data.append(json.loads(line))
     data.sort(key=lambda x: x['time_meas'])
     return data
+
+
+def filter_trajectory(data:list, is_in_area, lower_bound=10000, upper_bound=-1):
+    trajectories = dict()
+    for item in data:
+        position = json.loads(item['position'])
+        x = position['x']
+        y = position['y']
+
+        if is_in_area([x, y]) is False:
+            continue
+
+        if trajectories.get(item['id']) is None:
+            trajectories[item['id']] = [(x, y)]
+        else:
+            trajectories[item['id']].append((x, y))
+
+    filtered_trajectories = dict()
+    for i in trajectories.keys():
+        if len(trajectories[i]) > upper_bound or len(trajectories[i]) < lower_bound:
+            continue
+        filtered_trajectories[i] = trajectories[i]
+
+    return filtered_trajectories
